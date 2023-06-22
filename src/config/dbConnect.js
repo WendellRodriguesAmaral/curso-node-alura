beb58edb6445999604451955b2248cdeb8cb6e8f
Wendell_R <42788841+WendellRodriguesAmaral@users.noreply.github.com>
2023-06-21 21:13:39 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 //fazendo a conexão com o bd que criei no atlas
-mongoose.connect(`mongodb+srv://wendellrodrigues:123@curso-alura.uy02ud2.mongodb.net/alura-node`);
+mongoose.connect(``);
 
 //salvando numa variavel de conexão
 let db = mongoose.connection;
